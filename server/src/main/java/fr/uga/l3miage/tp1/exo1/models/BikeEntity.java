46e7ad6a8ed485b08cc9a9b11a8d1e64dab60ad0
l3miage-grasiaa <Angelica-Rivera.Grasia@etu.univ-grenoble-alpes.fr>
2024-03-14 20:44:02 +0100

    fini la class CarEntity.java et BikeEntity.java

---
+++ server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
@@ -0,0 +1,32 @@
+package fr.uga.l3miage.tp1.exo1.models;
+
+import fr.uga.l3miage.tp1.exo1.enums.ShifterType;
+import fr.uga.l3miage.tp1.exo1.enums.CylinderNumber;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+public class BikeEntity {
+    @Id
+    @Column(name="immatriculation")
+    private String immatriculation;
+
+    @Column(name="capacity")
+    private Integer capacity;
+
+    @Column(name="cylinder_number")
+    private CylinderNumber cylinderNumber;
+
+    @Column(name="automatic")
+    private Boolean automatic;
+
+    @Column(name="shifter")
+    private Boolean shifter;
+
+    @Column(name="shifter_type")
+    private ShifterType shifterType;
+
+    @Column(name="circulation_date")
+    private LocalDate circulationDate;
+
+}
