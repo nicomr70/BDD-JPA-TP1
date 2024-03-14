ca5ede94cda8272ac2b4a13b8e9fcd0a7c23e6b1
l3miage-pichouda <Adrien.Pichoud@etu.univ-grenoble-alpes.fr>
2024-03-14 18:11:35 +0100

    CarEntity

---
+++ server/src/main/java/fr/uga/l3miage/tp1/exo1/models/CarEntity.java
@@ -0,0 +1,31 @@
+package fr.uga.l3miage.tp1.exo1.models;
+
+import fr.uga.l3miage.tp1.exo1.enums.PowerType;
+import fr.uga.l3miage.tp1.exo1.enums.WeightUnity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="car_porshe")
+class CarEntity {
+    @Id
+    @Column(name="immat", length =8)
+    private String immatriculation;
+    @Column(name="cylinder_capacity")
+    private Double cylinderCapacity;
+    @Column (name="weight")
+    private Integer weight;
+    @Column (name="torque")
+    private Integer torque;
+    @Column (name="power")
+    private Integer power;
+    @Column(name="circulation_date")
+    private LocalDate circulationDate;
+    @Column (name="power_type")
+    @Enumerated(EnumType.ORDINAL)
+    private PowerType powerType;
+    @Column (name="weight_unity")
+    @Enumerated(EnumType.STRING)
+    private WeightUnity weightUnity;
+}
